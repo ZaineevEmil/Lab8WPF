134b2d3ab9f5f2d304132955285f71713d6ed9ed
2299112@gmail.com <2299112@gmail.com>
2021-12-25 21:13:48 +0500

    Добавьте файлы проекта.

---
+++ Lab8WPF/MyCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab8WPF
+{
+    class MyCommands
+    {
+        public static RoutedCommand Color { get; set; }
+
+        static MyCommands()
+        {
+            Color = new RoutedCommand("Color", typeof(MyCommands));
+        }
+
+    }
+}
